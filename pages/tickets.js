a1c1df471cd3cba13efe423e7c702dbd0611ae72
Adam Nuridov <adam.nuridov@bouvet.no>
2019-11-23 18:28:29 +0100

    tickets.js fixes

--- pages/tickets.js
+++ pages/tickets.js
@@ -1,75 +1,79 @@
 import Link from "next/link";
 import { activeTickets, expiredTickets } from "../data/tickets";
 import { timeConvert } from "../lib/utils";
-import Countdown from '../components/Countdown';
-import Swal from 'sweetalert2';
+import Countdown from "../components/Countdown";
+import Swal from "sweetalert2";
 
 const Ticket = ({ ticket }) => {
-    
-    function ticketTrainrideHasStarted(ticket) {
-        const trainStartTime = new Date(ticket.startTime);
-        const currentTime = new Date();
-        return trainStartTime < currentTime;
-    }
+  function ticketTrainrideHasStarted(ticket) {
+    const trainStartTime = new Date(ticket.startTime);
+    const currentTime = new Date();
+    return trainStartTime < currentTime;
+  }
 
-    function ticketTrainrideHasEnded(ticket) {
-        const trainEndTime = new Date(ticket.endTime);
-        const currentTime = new Date();
-        return trainEndTime < currentTime;
-    }
+  function ticketTrainrideHasEnded(ticket) {
+    const trainEndTime = new Date(ticket.endTime);
+    const currentTime = new Date();
+    return trainEndTime < currentTime;
+  }
 
-    //WIP
-    async function findTicketTrainTimeAlternatives(from, to, currentStartTime) {
-        fetchDepartures(from, to).then((results, err) => {
-            if (err) {
-              console.error(err)
-            }
-            console.log(results);
-            const trainTimeAlterantives = results.filter(obj => obj.startTime === currentStartTime);
-            console.log(trainTimeAlterantives);
-            }
-          );
-    }
+  //WIP
+  async function findTicketTrainTimeAlternatives(from, to, currentStartTime) {
+    fetchDepartures(from, to).then((results, err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(results);
+      const trainTimeAlterantives = results.filter(
+        obj => obj.startTime === currentStartTime
+      );
+      console.log(trainTimeAlterantives);
+    });
+  }
 
-    //WIP
-    function refundTicket(id) {
-        
-    }
+  //WIP
+  function refundTicket(id) {}
 
-    //WIP
-    const handleChangeTime = (e) => {
-        const parentNode = e.target.parentNode;
-        console.log("parent: "+parentNode);
-    }
+  //WIP
+  const handleChangeTime = e => {
+    const parentNode = e.target.parentNode;
+    console.log("parent: " + parentNode);
+  };
 
-    //WIP
-    const handleRefund = (e) => {
-        Swal.fire({
-            title: '<h2>Are you sure you want to refund this ticket?',
-            text: ticket.legs[0].fromPlace.name + " - " + ticket.legs[ticket.legs.length - 1].toPlace.name + ": " +
-            new Date(ticket.startTime).toLocaleTimeString("nb-NO", {hour: "2-digit", minute: "2-digit"})
-            + " - " +
-            new Date(ticket.endTime).toLocaleTimeString("nb-NO", {hour: "2-digit", minute: "2-digit"}),
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, refund'
-          }).then((result) => {
-            if (result.value) {
-              refundTicket(/*TODO: Need an id for deletion in tickets.json file */);  
-              Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-              )
-            }
-          })
-    }
+  //WIP
+  const handleRefund = e => {
+    Swal.fire({
+      title: "<h2>Are you sure you want to refund this ticket?",
+      text:
+        ticket.legs[0].fromPlace.name +
+        " - " +
+        ticket.legs[ticket.legs.length - 1].toPlace.name +
+        ": " +
+        new Date(ticket.startTime).toLocaleTimeString("nb-NO", {
+          hour: "2-digit",
+          minute: "2-digit"
+        }) +
+        " - " +
+        new Date(ticket.endTime).toLocaleTimeString("nb-NO", {
+          hour: "2-digit",
+          minute: "2-digit"
+        }),
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#360000",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, refund"
+    }).then(result => {
+      if (result.value) {
+        refundTicket(/*TODO: Need an id for deletion in tickets.json file */);
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  };
 
-    return (
-        <>
-            <style jsx>{`
+  return (
+    <>
+      <style jsx>{`
         li {
           border: 2px solid #360000;
           width: 330px;
@@ -77,70 +81,71 @@ const Ticket = ({ ticket }) => {
           margin-left: 1em;
           padding: 1em;
         }
-        li:focus-within {
-          background: #360000;
-        }
-        a {
-          text-decoration: none;
-          color: #2b2b2b;
-        }
-        a:focus,
-        li:focus-within h3,
-        li:focus-within small {
-          outline: 0;
-          color: #fafafa;
-        }
         h3 {
           margin-top: 0;
         }
         small.price {
           float: right;
         }
+        button {
+          height: 50px;
+          font-size: 16px;
+          font-weight: bold;
+          padding: 0 10px;
+          background-color: #360000;
+          color: white;
+          cursor: pointer;
+        }
+        button:last-child {
+            float: right;
+        }
+        button:focus {
+          background-color: white;
+          color: #360000;
+          -webkit-appearance: none;
+          outline: 4px solid;
+        }
       `}</style>
-            <li>
-                <Link href="#">
-                    <a>
-                        <h3>
-                            {ticket.legs[0].fromPlace.name} -{" "}
-                            {ticket.legs[ticket.legs.length - 1].toPlace.name}
-                        </h3>
-                        <p>
-                            {new Date(ticket.startTime).toLocaleTimeString("nb-NO", {
-                                hour: "2-digit",
-                                minute: "2-digit"
-                            })}{" "}
-                            -{" "}
-                            {new Date(ticket.endTime).toLocaleTimeString("nb-NO", {
-                                hour: "2-digit",
-                                minute: "2-digit"
-                            })}
-                        </p>
-                        <small>{timeConvert(ticket.duration)}</small>
-                        <small className="price">
-                            {Math.round(ticket.duration / 120)} NOK
-                        </small>
-                        {ticketTrainrideHasStarted(ticket) === false && ticketTrainrideHasEnded(ticket) === false &&
-                            <>
-                                <hr />
-                                
-                                <p>
-                                    Remaining time for changes: <Countdown dateAsString={ticket.startTime}/>
-                                </p>
-                                <button onClick={handleChangeTime}>Change date/time</button>
-                                <button onClick={handleRefund}>Refund ticket</button>
-                            </>
-                        }
-                    </a>
-                </Link>
-            </li>
-        </>
-    );
+      <li>
+        <h3>
+          {ticket.legs[0].fromPlace.name} -{" "}
+          {ticket.legs[ticket.legs.length - 1].toPlace.name}
+        </h3>
+        <p>
+          {new Date(ticket.startTime).toLocaleTimeString("nb-NO", {
+            hour: "2-digit",
+            minute: "2-digit"
+          })}{" "}
+          -{" "}
+          {new Date(ticket.endTime).toLocaleTimeString("nb-NO", {
+            hour: "2-digit",
+            minute: "2-digit"
+          })}
+        </p>
+        <small>{timeConvert(ticket.duration)}</small>
+        <small className="price">{Math.round(ticket.duration / 120)} NOK</small>
+        {ticketTrainrideHasStarted(ticket) === false &&
+          ticketTrainrideHasEnded(ticket) === false && (
+            <>
+              <hr />
+
+              <p>
+                Remaining time for changes:{" "}
+                <Countdown dateAsString={ticket.startTime} />
+              </p>
+              <button onClick={handleChangeTime}>Change date/time</button>
+              <button onClick={handleRefund}>Refund ticket</button>
+            </>
+          )}
+      </li>
+    </>
+  );
 };
 
 const Tickets = () => {
-    return (
-        <>
-            <style jsx>{`
+  return (
+    <>
+      <style jsx>{`
         ul {
           list-style: none;
           display: flex;
@@ -148,31 +153,31 @@ const Tickets = () => {
           flex-wrap: wrap;
         }
         h2 {
-            text-align: center;
+          text-align: center;
         }
       `}</style>
-            {activeTickets.length > 0 && (
-                <>
-                    <h2>Active Tickets</h2>
-                    <ul>
-                        {activeTickets.map((ticket, idx) => (
-                            <Ticket key={idx} ticket={ticket} />
-                        ))}
-                    </ul>
-                </>
-            )}
-            {expiredTickets.length > 0 && (
-                <>
-                    <h2>Expired Tickets</h2>
-                    <ul>
-                        {expiredTickets.map((ticket, idx) => (
-                            <Ticket key={idx} ticket={ticket} />
-                        ))}
-                    </ul>
-                </>
-            )}
+      {activeTickets.length > 0 && (
+        <>
+          <h2>Active Tickets</h2>
+          <ul>
+            {activeTickets.map((ticket, idx) => (
+              <Ticket key={idx} ticket={ticket} />
+            ))}
+          </ul>
         </>
-    );
+      )}
+      {expiredTickets.length > 0 && (
+        <>
+          <h2>Expired Tickets</h2>
+          <ul>
+            {expiredTickets.map((ticket, idx) => (
+              <Ticket key={idx} ticket={ticket} />
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Tickets;
