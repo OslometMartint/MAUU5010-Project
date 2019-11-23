b225e06719f37550bff37f52a67dd7d1903dab4f
Adam Nuridov <adam.nuridov@bouvet.no>
2019-11-23 14:10:48 +0100

    merge fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import StationSearchBar from '../components/StationSearchbar';
-import { timeConvert, fetchDepartures } from '../lib/utils';
-import Swal from 'sweetalert2';
+import { useState } from "react";
+import StationSearchForm from "../components/StationSearchForm";
+import { timeConvert, expireTickets } from "../lib/utils";
+
+import Swal from "sweetalert2";
 
 const debounce = (fn, delay) => {
   let timer = null;
@@ -35,91 +36,64 @@ const Index = () => {
     Swal.fire(
       "Congratulations!",
       `You have bought a ticket from ${fromValue} to ${toValue}!`,
-      'success')
-  }
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    if (isFromValueValid && isToValueValid) {
-      updateDepartures();
-      console.log(departures);
-    }
-  }
-
-  async function updateDepartures() {
-    fetchDepartures(fromValue, toValue).then((results, err) => {
-      if (err) {
-        console.error(err)
-      }
-      setDepartures(results.filter(obj => obj.legs.every(leg => leg.mode === 'rail')));
-      }
+      "success"
     );
-  }
+  };
 
   return (
-    <div class="container">
-      <form onSubmit={handleOnSubmit} action="">
-        <label for="fromStationSearchBar">From:</label>
-        <StationSearchBar id="fromStationSearchBar" setValue={setFromValue} value={fromValue} valid={setFromValueValid} />
-        <label for="toStationSearchBar">To:</label>
-        <StationSearchBar id="toStationSearchBar"setValue={setToValue} value={toValue} valid={setToValueValid} />
-        <button>Show departures</button>
-      </form>
-      <div class="departuresContainer">
-        {departures.length > 0 &&
-          <>
-            <h2>Departures</h2>
-            <ul role="list">
-              {departures.map((departure, idx) =>
-                <li key={idx}>
-                  <h3>{departure.startTime.slice(11, 16)} - {departure.endTime.slice(11, 16)}</h3>
-
-                  {timeConvert(departure.duration)}
-                  Pris: {Math.round(departure.duration / 120)},-
-                  <button onClick={() => buyTicket(departure)}>Buy ticket</button>
-                </li>)
-              }
-            </ul>
-          </>
-        }
-      </div>
+    <div>
+      <StationSearchForm 
+        setFromValue={setFromValue} 
+        fromValue={fromValue}
+        setToValue={setToValue} 
+        toValue={toValue}
+        setDepartures={setDepartures}
+      />
+      {departures.length > 0 && (
+        <>
+          <h2>Departures</h2>
+          <ul role="list">
+            {departures.map((departure, idx) => (
+              <li key={idx}>
+                <h3>
+                  {departure.startTime.slice(11, 16)} -{" "}
+                  {departure.endTime.slice(11, 16)}
+                </h3>
+                {timeConvert(departure.duration)}
+                Price: {Math.round(departure.duration / 120)},-
+                <button onClick={() => buyTicket(departure)}>Buy ticket</button>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
       <style jsx>{`
-      ul {
-        list-style: none;
-      }
-      li {
-        border: 2px solid #360000;
-        width: 330px;
-        margin-bottom: 1em;
-        padding: 1em;
-      }
-      li::focus-within {
+        ul {
+          list-style: none;
+        }
+
+        li {
+          border: 2px solid #360000;
+          width: 330px;
+          margin-bottom: 1em;
+          padding: 1em;
+        }
+        li::focus-within {
           blackground-color: #360000;
           color: #fafafa;
-      }
-      a {
+        }
+        a {
           text-decoration: none;
-          color: #2B2B2B;
-      }
-      a:focus {
+          color: #2b2b2b;
+        }
+        a:focus {
           outline: 0;
-      }
-      h3 {
+        }
+        h3 {
           margin-top: 0;
           float: right;
-      }
-      button {
-        color: #fafafa;
-        background-color: #360000;
-        border: 0;
-      }
-      .container {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: flex-end;
-        flex-direction: row;
-      }
-    `}</style>
+        }
+      `}</style>
     </div>
   );
 };
